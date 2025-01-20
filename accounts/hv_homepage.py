8a8da8fd6f723cc601aa7ff988cb9ec500725424
bizbuddydev <bizbuddydev@gmail.com>
2025-01-19 18:54:22 -0800

    Update hv_homepage.py

--- accounts/hv_homepage.py
+++ accounts/hv_homepage.py
@@ -424,6 +424,9 @@ def main():
     # Pull data using the function
     account_data = pull_dataframes(DATASET_ID, ACCOUNT_TABLE_ID)
     post_data = pull_dataframes(DATASET_ID, POST_TABLE_ID)
+
+    st.write(post_data)
+    
     post_data = post_data.sort_values(by='created_time', ascending=True)
     post_data = post_data[post_data['insert_date'] == yesterday]
 
