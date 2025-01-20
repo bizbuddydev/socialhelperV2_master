a107d500178023502f0d7f6c6dff468ce08f160f
bizbuddydev <bizbuddydev@gmail.com>
2025-01-19 21:09:39 -0800

    Update hv_homepage.py

--- accounts/hv_homepage.py
+++ accounts/hv_homepage.py
@@ -647,7 +647,7 @@ def main():
     
             # Add past posts from the post_data dataframe to the calendar
             for _, row in post_data.iterrows():
-                st.write(row['created_time'].strftime('%Y-%m-%d'))
+                st.write(row)
                 st.session_state["calendar_events"].append({
                     "title": f"Past Post: {row['caption'][:10]}",
                     "start": row['created_time'].strftime('%Y-%m-%d'),
