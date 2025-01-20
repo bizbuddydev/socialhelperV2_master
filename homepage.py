04d1fd844cc1b9ea839e27b9c88270d992d52435
bizbuddydev <bizbuddydev@gmail.com>
2025-01-19 16:53:33 -0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -424,7 +424,7 @@ def main():
     account_data = pull_dataframes(DATASET_ID, ACCOUNT_TABLE_ID)
     post_data = pull_dataframes(DATASET_ID, POST_TABLE_ID)
     post_data = post_data.sort_values(by='created_time', ascending=True)
-    post_data = post_data[['insert_date'] == yesterday]
+    post_data = post_data[post_data['insert_date'] == yesterday]
 
     # Get daily posts
     account_data = get_daily_post_counts(post_data, account_data)
