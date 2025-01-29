9ee2cb168e2fd476bd05d209688aa38228a5a522
bizbuddydev <bizbuddydev@gmail.com>
2025-01-28 21:30:15 -0800

    Update post_deepdive.py

--- post_deepdive.py
+++ post_deepdive.py
@@ -3,8 +3,30 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 import pandas as pd
 from datetime import date, timedelta
+import json
 
 st.set_page_config(page_title="Post Analyzer", layout="wide", page_icon="📱")
+
+# Define links to other pages
+PAGES = {
+    "📊 Overview": "https://smp-bizbuddy-accountoverview.streamlit.app/",
+    "📱 Posts": "https://smp-bizbuddy-postoverview.streamlit.app",
+    "🗓️ Scheduler": "https://smp-bizbuddy-postscheduler.streamlit.app/",
+    "💡 Brainstorm": "https://smp-bizbuddy-v1-brainstorm.streamlit.app/"
+}
+
+# Sidebar navigation
+st.sidebar.title("Navigation")
+for page, url in PAGES.items():
+    st.sidebar.markdown(f"[**{page}**]({url})", unsafe_allow_html=True)
+
+# Load the configuration file
+def load_config(file_path="config.json"):
+    with open(file_path, "r") as f:
+        return json.load(f)
+
+# Load the account configuration
+config = load_config()
 
 # Load credentials and project ID from st.secrets
 credentials = service_account.Credentials.from_service_account_info(
@@ -15,24 +37,40 @@ project_id = st.secrets["gcp_service_account"]["project_id"]
 # Function to fetch data from BigQuery
 def fetch_data(query: str) -> pd.DataFrame:
     client = bigquery.Client(credentials=credentials, project=project_id)
-    query_job = client.query(query)
-    result = query_job.result()
+    query_job = client.query(query)  # Make a query request
+    result = query_job.result()  # Wait for the query to finish
     return result.to_dataframe()
 
-# Define queries
-QUERY = '''
+# Define filter functions
+def filter_last_30_days(df):
+    cutoff = date.today() - timedelta(days=30)
+    return df[df["created_time"] >= cutoff].sort_values(by="created_time", ascending=False)
+
+def filter_last_6_months(df):
+    cutoff = date.today() - timedelta(days=182)  # Approx. 6 months
+    return df[df["created_time"] >= cutoff].sort_values(by="created_time", ascending=False)
+
+def top_10_by_column(df, column):
+    return df.sort_values(by=column, ascending=False).head(10)
+
+# Use the variables in your app
+account_name = config["ACCOUNT_NAME"]
+datasetid = config["TESTING_DATASET_ID"]
+tableid = config["ANALYSIS_TABLE_ID"]
+
+### Get data ###
+query = f"""
 SELECT *
-FROM `bizbuddydemo-v1.${datasetid}.${tableid}`
+FROM `bizbuddydemo-v1.{datasetid}.{tableid}`
 ORDER BY created_time DESC
-'''
+"""
 
 # Load/Transform Data
-data = fetch_data(QUERY)
-data["Like Rate"] = round(data["like_count"] / data["reach"] * 100, 2)
-data["created_time"] = pd.to_datetime(data["created_time"]).dt.date
+data = fetch_data(query)
+
 
 def main():
-    st.title("Social Buddy - Post Analyzer")
+    st.title("Social Buddy - Post Deep Dive")
 
     # Filtering Options
     st.sidebar.header("Filter Posts")
