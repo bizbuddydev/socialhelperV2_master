4bf676142681b3af4743f29d6729d7acf60c103e
bizbuddydev <bizbuddydev@gmail.com>
2025-01-29 22:37:32 -0800

    Update post_overview.py

--- post_overview.py
+++ post_overview.py
@@ -84,7 +84,7 @@ data["Like Rate"] = round(data["like_count"]/data["reach"] * 100, 2)
 data["created_time"] = pd.to_datetime(data["created_time"]).dt.date
 
 # Get analyzed posts data and merge
-ap_data = fetch_Data(ap_query)
+ap_data = fetch_data(ap_query)
 
 merged_data = data.merge(ap_data, left_on="post_id", right_on="video_id", how="left")
 
