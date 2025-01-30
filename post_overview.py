90379bcb037f564475e77bbb7d88497bdb43c521
bizbuddydev <bizbuddydev@gmail.com>
2025-01-29 23:10:11 -0800

    Update post_overview.py

--- post_overview.py
+++ post_overview.py
@@ -243,7 +243,7 @@ def main():
                 <div class="scorecard">Time of Day: {row['time_bucket']}</div>
                 <div class="scorecard">Weekday: {row['weekday']}</div>
                 <div class="scorecard">Speech Length: {row['speech_length']} words</div>
-                <div class="scorecard">Speech Rate: {round(row['speech_rate'])} words per min</div>
+                <div class=f"scorecard">Speech Rate: {row['speech_rate']}.2f words per min</div>
             </div>
             """
 
