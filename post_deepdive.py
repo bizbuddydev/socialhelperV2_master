06be3bf09d477c850675db04f42c2fef19588ba0
bizbuddydev <bizbuddydev@gmail.com>
2025-01-30 00:25:14 -0800

    Update post_deepdive.py

--- post_deepdive.py
+++ post_deepdive.py
@@ -10,13 +10,15 @@ import json
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-st.set_page_config(page_title="Post Analyzer 🚀", layout="wide", page_icon="📱")
+st.set_page_config(page_title="Post Analyzer 🚀", layout="wide", page_icon="📡")
 
 # Define links to other pages
 PAGES = {
-    "📊 Overview": "https://smp-bizbuddy-accountoverview.streamlit.app/",
-    "📱 Posts": "https://smp-bizbuddy-postoverview.streamlit.app",
-    "🗓️ Scheduler": "https://smp-bizbuddy-postscheduler.streamlit.app/",
+    "📊 Overview": "https://smp-bizbuddyv2-homepage.streamlit.app/",
+    "📱 Posts": "https://smp-bizbuddyv2-postoverview.streamlit.app/",
+    # "🗓️ Scheduler": "https://smp-bizbuddy-postscheduler.streamlit.app/",
+    "📡 Deep Dive": "https://smp-bizbuddyv2-postoverview.streamlit.app/",
+    "🚝 Inspiration Upload": "https://smp-bizbuddyv2-inspoupload.streamlit.app/",
     "💡 Brainstorm": "https://smp-bizbuddy-v1-brainstorm.streamlit.app/"
 }
 
