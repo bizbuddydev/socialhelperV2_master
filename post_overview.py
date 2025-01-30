c919e829cac2c57f7145555913d691328d52b57d
bizbuddydev <bizbuddydev@gmail.com>
2025-01-29 23:12:59 -0800

    Update post_overview.py

--- post_overview.py
+++ post_overview.py
@@ -212,8 +212,9 @@ def main():
             # Display created_time_posts
             st.markdown(f"<div class='details'>Posted On: {row['created_time_posts']}</div>", unsafe_allow_html=True)
     
-            # Display caption with title
-            st.markdown(f"<div class='caption'>Caption: {row['caption_posts']}</div>", unsafe_allow_html=True)
+            with st.expander("See post caption"):
+                # Display caption with title
+                st.markdown(f"<div class='caption'>Caption: {row['caption_posts']}</div>", unsafe_allow_html=True)
             
             # Display metrics in scorecards
             metrics_html = f"""
