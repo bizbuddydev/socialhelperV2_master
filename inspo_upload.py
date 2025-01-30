5d60454232f634aaca932a6a12e3a606886153f9
bizbuddydev <bizbuddydev@gmail.com>
2025-01-30 00:25:25 -0800

    Update inspo_upload.py

--- inspo_upload.py
+++ inspo_upload.py
@@ -5,7 +5,7 @@ import pandas as pd
 from datetime import date, timedelta
 import json
 
-st.set_page_config(page_title="Post Analyzer", layout="wide", page_icon="📱")
+st.set_page_config(page_title="Post Analyzer", layout="wide", page_icon="🚝")
 
 # Define links to other pages
 PAGES = {
