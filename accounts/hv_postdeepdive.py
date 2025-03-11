52e1e94f986bc04c57530ca074cd3f0e5d0aa2ec
bizbuddydev <bizbuddydev@gmail.com>
2025-03-11 00:19:43 -0700

    Update hv_postdeepdive.py

--- accounts/hv_postdeepdive.py
+++ accounts/hv_postdeepdive.py
@@ -43,16 +43,16 @@ def assign_time_buckets(df):
 
     # Define bucket mapping
     def bucketize(hour):
-        if 9 <= hour <= 11:
+        if 9 <= hour < 12:  # 9 AM - 11 AM
             return f"{hour} AM"
-        elif hour == 12:
+        elif hour == 12:  # Noon case
             return "12 PM"
-        elif 13 <= hour <= 23:
+        elif 13 <= hour <= 23:  # Convert 13-23 (1 PM - 11 PM) correctly
             return f"{hour - 12} PM"
-        elif hour == 0:
+        elif hour == 0:  # Midnight
             return "12 AM"
-        else:  # Covers 1 AM - 7 AM
-            return "1-7 AM"
+        else:  # 1-8 AM
+            return "1-8 AM"
 
     # Assign time buckets
     df["time_bucket"] = df["hour"].apply(bucketize)
@@ -60,7 +60,7 @@ def assign_time_buckets(df):
     # Define categorical ordering
     time_bucket_order = [
         "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM",
-        "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1-7 AM"
+        "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1-8 AM"
     ]
     weekday_order = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
@@ -72,7 +72,7 @@ def assign_time_buckets(df):
     df = df.drop(columns=["hour"])
 
     return df
-
+    
 ### **Fetch Post Data (Filtered by Page ID)**
 datasetid = config["DATASET_ID"]
 post_tableid = config["POST_TABLE_ID"]
